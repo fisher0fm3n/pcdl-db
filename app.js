29d1d061bf7e08dcbe19dcf6f5317835f5aca5b6
fisher0fm3n <deszeytutorials@gmail.com>
2022-11-04 16:37:29 +0000

    Update database

--- app.js
+++ app.js
@@ -177,6 +177,142 @@ const Series = [
      }
     ],
   },
+  {
+    id: 'z6hkbui',
+    SeriesAuthor: 'Bro Desmond',
+    SeriesAuthorEmail: 'gardrox@gmail.com',
+    SeriesStatus: 'Published',
+    SeriesLastUpdated: '2012-04-23T18:25:43.511Z',
+    SeriesCreation: '2012-04-23T18:25:43.511Z',
+    SeriesTitle: 'The Power and Pursuit of Purpose',
+    SeriesDescription: 'You shall receive power after the Holy Ghost comes upon you.',
+    SeriesCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+    SeriesCaptions: ['English', 'French'],
+    SeriesTags: [
+      'holy_spirit',
+      'christian_living',
+      'faith',
+    ],
+    Seasons: [
+     {
+      // SeriesID: 'bni8yt6',
+      SeasonID: 0,
+      SeasonTitle: 'Volume 1',
+      SeasonStatus: 'Published',
+      SeasonCreation: '2012-04-23T18:25:43.511Z',
+      SeasonLastUpdated: '2012-04-23T18:25:43.511Z',
+      SeasonAuthor: 'Bro Desmond',
+      Messages: [
+        {
+          // SeasonID: 0,
+          MessageID: 'kdg7em',
+          MessageTitle: 'The Power and Pursuit of Purpose Part 1',
+          MessageDescription: 'What is your purpose in life? Why do you do what you do? What’s your driving force? There are those who are motivated by anger, resentment, guilt and the desire for approval and because of these they never do much with God. The Word of God is the true light that tells right from wrong. In this special teaching by Pastor Chris Oyakhilome DSc, DD, you’ll get to understand how to live for a higher purpose that is beyond this world.',
+          MessageCaptions: ['English', 'French'],
+          MessageStatus: 'Published',
+          MessageCreation: '2012-04-23T18:25:43.511Z',
+          MessageLastUpdated: '2012-04-23T18:25:43.511Z',
+          MessageAuthor: 'Bro Desmond',
+          MessageCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+          MessagePoster: 'https://i.postimg.cc/FHrs7QQ9/Screenshot-20180719-1001102-660x330.png',
+          MessageMedia: {
+            video: {
+              AlbumPrice: 10,
+              AlbumID: '443294a3f31c4742657232124dfbcaf7',
+            }
+          },
+        },
+        {
+          // SeasonID: 0,
+          MessageID: 'nh7l9l',
+          MessageTitle: 'The Power and Pursuit of Purpose Part 2',
+          MessageDescription: 'Did you know that everything about your outward appearance was given to you so that you can communicate with this world and fight the fight of faith? If any man be in Christ, he is a new creature; one who never existed before with a constantly changing glory by the Word. Find out from this series on the Power and Pursuit of Purpose how to walk in the light of the Word that causes you to fulfill the will of Father for your life.',
+          MessageStatus: 'Published',
+          MessageCreation: '2012-04-23T18:25:43.511Z',
+          MessageLastUpdated: '2012-04-23T18:25:43.511Z',
+          MessageAuthor: 'Bro Desmond',
+          MessageCaptions: ['English', 'French'],
+          MessageCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+          MessagePoster: 'https://i.postimg.cc/FHrs7QQ9/Screenshot-20180719-1001102-660x330.png',
+          MessageMedia: {
+            video: {
+              AlbumPrice: 10,
+              AlbumID: '476676758c7016ef31f6ba2c7fb52bf0',
+            }
+          },
+        }
+      ],
+     },
+     {
+      // SeriesID: 'bni8yt6',
+      SeasonID: 1,
+      SeasonTitle: 'Volume 2',
+      SeasonStatus: 'Published',
+      SeasonCreation: '2012-04-23T18:25:43.511Z',
+      SeasonLastUpdated: '2012-04-23T18:25:43.511Z',
+      SeasonAuthor: 'Bro Desmond',
+      Messages: [
+        {
+          // SeasonID: 0,
+          MessageID: 'ondug9',
+          MessageTitle: 'The Power and Pursuit of Purpose (Communication and Purity)',
+          MessageDescription: 'In our pursuit of purpose, the Lord has put his wisdom in our mouths, which the adversaries will not be able to gainsay nor resist. In this teaching, Pastor Chris Oyakhilome DSc, DD expounds on the value of communicating clearly and with purity, so that everyone would read and understand quickly the gospel. Get to know from this message how a Christian can live victoriously in this life.',
+          MessageCaptions: ['English', 'French'],
+          MessageStatus: 'Published',
+          MessageCreation: '2012-04-23T18:25:43.511Z',
+          MessageLastUpdated: '2012-04-23T18:25:43.511Z',
+          MessageAuthor: 'Bro Desmond',
+          MessageCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+          MessagePoster: 'https://i.postimg.cc/FHrs7QQ9/Screenshot-20180719-1001102-660x330.png',
+          MessageMedia: {
+            video: {
+              AlbumPrice: 10,
+              AlbumID: '275341211b4e929b1e18818a9ad03073',
+            }
+          },
+        },
+        {
+          // SeasonID: 0,
+          MessageID: '9jbofu',
+          MessageTitle: 'The Power and Pursuit of Purpose (The Power of Resources, Faith and Focus)',
+          MessageDescription: 'What shall it profit a man, if he shall gain the whole world and lose his soul? What shall a man give in exchange for his soul (Mark 8:36-37)? Real satisfaction is in the soul, not in the brain or money. When your soul is in oneness with God, you can rejoice in His accomplishments through you. You will understand from this message by Pastor Chris, the Power of Resources, Faith and Focus in fulfilling your purpose in the kingdom.',
+          MessageCaptions: ['English', 'French'],
+          MessageStatus: 'Published',
+          MessageCreation: '2012-04-23T18:25:43.511Z',
+          MessageLastUpdated: '2012-04-23T18:25:43.511Z',
+          MessageAuthor: 'Bro Desmond',
+          MessageCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+          MessagePoster: 'https://i.postimg.cc/FHrs7QQ9/Screenshot-20180719-1001102-660x330.png',
+          MessageMedia: {
+            video: {
+              AlbumPrice: 10,
+              AlbumID: '0fddb714e35e2e19d1bbfb88bfbfa29b',
+            }
+          },
+        },
+        {
+          // SeasonID: 0,
+          MessageID: '7hb9u',
+          MessageTitle: 'The Power and Pursuit of Purpose (The Power of Love)',
+          MessageDescription: 'Did you know that Jesus cares whether or not you love Him? The scriptures tell us the one who loves Jesus will keep his word (John 14:23).  Your life is made by the Word and for the Word. Find out from this teaching by Pastor Chris Oyakhilome DSc, DD, what your purpose in life is and how to fulfill God’s plan for your life as you walk in the light of the Word of God.',
+          MessageCaptions: ['English', 'French'],
+          MessageStatus: 'Published',
+          MessageCreation: '2012-04-23T18:25:43.511Z',
+          MessageLastUpdated: '2012-04-23T18:25:43.511Z',
+          MessageAuthor: 'Bro Desmond',
+          MessageCover: 'https://dr9op7qw3y7b0.cloudfront.net/albums/64f7e2fee067a5414a183b812a96232b/the_power_and_pursuit_of_purpose.jpg',
+          MessagePoster: 'https://i.postimg.cc/FHrs7QQ9/Screenshot-20180719-1001102-660x330.png',
+          MessageMedia: {
+            video: {
+              AlbumPrice: 10,
+              AlbumID: 'c9ebb43d929826c9fa583e6d0957a89c',
+            }
+          },
+        }
+      ],
+     }
+    ],
+  }
 ];
 
 const playlists = [
